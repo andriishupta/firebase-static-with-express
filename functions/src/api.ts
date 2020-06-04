05f4eedff4aa0eeda23def7ba0ef3884573b1214
Andrii Shupta <andrew260794@gmail.com>
2020-06-04 23:04:35 +0300

    build(functions): update index.html build;

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -7,11 +7,15 @@ admin.initializeApp();
 const router = express.Router();
 
 router.get('/health', (req, res) => {
-  return res.json({ message: 'Api functions are running!' });
+  return res.json({ message: 'api functions are running!' });
 });
 
 router.get('/projects', (req, res) => {
   return res.json([1 , 2 , 3]);
+});
+
+router.get('**', (req, res) => {
+  return res.status(400).json({ error: 'bad request', code: 400 });
 });
 
 export default router;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,19 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as path from 'path';
+import * as fs from 'fs';
 import _api from './api'
 
 const _app = express();
 
+const html = fs.readFileSync(path.join(__dirname, 'index.html'));
+
 _app.use('/api', _api);
 
 _app.get('/', (req, res) => {
-  res.setHeader('Cache-control', 'public, max-age=600, s-maxage=1200')
-  res.sendFile(path.join(__dirname, '..', '..', 'app', 'index.html'));
+  res.setHeader('cache-control', 'public, max-age=600, s-maxage=1200');
+  res.setHeader('content-type', 'text/html');
+  res.end(html);
 });
 
 export const app = functions.https.onRequest(_app);
